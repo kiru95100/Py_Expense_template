a156801913c0fb09557e00eba020e56792061fcb
Kiruthika Tanigassalame <kiruthika.tanigassalame@epita.fr>
2023-09-20 21:59:14 +0200

    started the user question ...

--- user.py
+++ user.py
@@ -1,8 +1,14 @@
 from PyInquirer import prompt
 user_questions = [
 
+{
+        "type":"input",
+        "name":"name",
+        "message":"Quel est votre nom et prénom? ",
+    }
 ]
 
 def add_user():
     # This function should create a new user, asking for its name
+    infos = prompt(user_questions)
     return
